a4421ef307d8846c79bc1a8cfb52489d701bcc1d
unknown <richasharma535270@gmail.com>
2022-04-27 01:09:26 +0530

    blog

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://FunctionUp-cohort-1:TENBQZypGNobyoq3@cluster0.7jbgo.mongodb.net/project1-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://FunctionUp:surbhi231@cluster0.ufu0v.mongodb.net/project1-Blog", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const CreateBlog=require("../Controllers/blogs")
 
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+router.post("/CreateBlog",CreateBlog.CreateBlog)
 
 
 module.exports = router;
